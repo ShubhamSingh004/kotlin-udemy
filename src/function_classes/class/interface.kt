2e03a2a5b6b3f75e64f67f355b43fb115ed102eb
Shubham <shubhamsinghmys@gmail.com>
2025-04-19 13:27:46 +0530

    abstract function & class

--- src/function_classes/class/interface.kt
+++ src/function_classes/class/interface.kt
@@ -1,6 +1,8 @@
 package function_classes.`class`
 
 interface Archery {
+    fun height()    // abstract function
+
     fun archer(){
         println("Archer Skill")
     }
@@ -20,16 +22,28 @@ interface Singer{
     }
 }
 
-class Child: Archery, Singer{
+
+// make Child abstract or override height()
+class Child: Archery, Singer, BaseClass() {
     override fun personality() {    // mandatory override
         super<Archery>.personality()    // or super<Singer>.personality()
+    }
+
+    override fun singing() {
+        println("Classical singing")
+    }
+
+    override fun height() {  // mandatory override
+        println("5'10 feet")
     }
 }
 
 fun main(){
     val obj = Child()
-    obj.archer()
-    obj.singing()
+    obj.archer()    // Archer Skill
+    obj.singing()   // Classical singing
 
-    obj.personality()
+    obj.personality()   // Jolly
+    obj.role()  // Role of base class
+    obj.height()    // 5'10 feet
 }
